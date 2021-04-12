3cad242d73e8244a4da05c8bd1db904c2e805a6a
EvanBranton <ebranton@uncc.edu>
2021-04-12 13:26:58 -0400

    -Added a Temporary SQLConnection for Test of the LeaderBoard Service -Added LeaderBoardService to generate a leaderboard (For now does top 10 but will do everything eventually -Added Leaderboard model for getting for future storage in the Database

---
+++ BrainiacBattle/Services/SQLConnection.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace BrainiacBattle.Services
+{
+
+    public class DatabaseConnection
+    {
+        private string cString = "Server=tcp:braniacbattle.database.windows.net,1433;Initial Catalog=BrainiacBattle;Persist Security Info=False;User ID=RuntimeTerror;Password=RTRules@97;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        private SqlConnection con;
+        private SqlCommand cmd;
+        private SqlDataReader reader;
+        public DatabaseConnection()
+        {
+            con = new SqlConnection(cString);
+
+        }
+        public void OpenDatabase()
+        {
+            con.Open();
+        }
+        public void CloseDatabase()
+        {
+            con.Close();
+        }
+        public SqlConnection GetSqlConnection()
+        {
+            return con;
+        }
+        public void SetSqlCommand(string qString)
+        {
+            cmd = new SqlCommand(qString, con);
+        }
+        public SqlCommand GetSqlCommand()
+        {
+            return cmd;
+        }
+        public void SetDataReader()
+        {
+            reader = cmd.ExecuteReader();
+        }
+
+        public SqlDataReader GetDataReader()
+        {
+            return reader;
+        }
+    }
+}
